8019afcf3fb656eb56fdd15907958e67bd2b96d1
Apoorva Gondane <62504209+Moneybee985@users.noreply.github.com>
2022-12-12 18:47:02 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,37 +6,37 @@ const projectcards = document.querySelector(".projectcards");
 // Array of object for projects
 const projects = [
   {
-    title: "Quiz App",
+    title: "Covid-19 Detection using Cough Audio",
     cardImage: "assets/images/project-page/quiz.jpg",
-    description: "A quiz app built using basic react.",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
+    description: "•	Developing a Covid-19 Detection application using Cough Audio which takes in cough audio sample as input and classifies it into three categories: positive, negative and symptomatic.",
+    //tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
+    //Previewlink: "",
+    //Githublink: "",
   },
   {
-    title: "Chess Engine",
+    title: "Histopathologic Cancer Detection",
     cardImage: "assets/images/project-page/chess.jpg",
-    description: "A chess engine for a popular game dev engine.",
+    /*description: "A chess engine for a popular game dev engine.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "",*/
   },
   {
-    title: "Flappy Bird",
-    cardImage: "assets/images/project-page/flappybird.png",
+    title: "Business Intelligence Project on Spotify",
+    cardImage: "assets/images/project-page/Dashboard.png",
     description: "Flappy bird game built using React.js",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
+    Previewlink: "https://moneybee985.github.io/Building-Business-Intelligence-Project-using-Power-BI-on-Spotify/",
+    Githublink: "https://github.com/Moneybee985/Building-Business-Intelligence-Project-using-Power-BI-on-Spotify",
   },
   {
-    title: "Exercise Tracker",
-    cardImage: "assets/images/project-page/exercise.jpg",
+    title: "Exploratory Data Analysis: Airbnb",
+    cardImage: "assets/images/project-page/Airbnb ntbk pic.jpg",
     description: "Exercise tracker built using basic redux.",
     tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
     Previewlink: "",
     Githublink: "",
-  },
+  },/*
   {
     title: "Recipe Ingredient Parser",
     cardImage: "assets/images/project-page/recipe.jpg",
@@ -108,7 +108,7 @@ const projects = [
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
     Githublink: "",
-  },
+  },*/
 ];
 
 // function for rendering project cards data
